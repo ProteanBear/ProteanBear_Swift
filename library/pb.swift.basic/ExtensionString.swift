1ef1769a8f5622a534d9cd62bf821f03167c5f7d
ProteanBear <moru_1982@hotmail.com>
2016-05-04 10:26:54 +0800

    增加文字尺寸计算方法

--- library/pb.swift.basic/ExtensionString.swift
+++ library/pb.swift.basic/ExtensionString.swift
@@ -10,4 +10,20 @@ import Foundation
 
 extension String
 {
+    //计算文字的尺寸
+    public func pbTextSize(size:CGSize,font:UIFont) -> CGSize
+    {
+        let attribute = [NSFontAttributeName: font]
+        let rect=NSString(string:self).boundingRectWithSize(size, options: .UsesLineFragmentOrigin, attributes: attribute, context: nil)
+        return CGSize(width: rect.width, height: rect.height)
+    }
+    
+    //计算文字的高度
+    public func pbTextHeight(width:CGFloat,font:UIFont) -> CGFloat
+    {
+        let attribute = [NSFontAttributeName: font]
+        let size=CGSize(width: width, height: 0)
+        let rect=NSString(string:self).boundingRectWithSize(size, options: .UsesLineFragmentOrigin, attributes: attribute, context: nil)
+        return rect.height
+    }
 }
